d6f73a7abbc12e57580830092a2805f64c07ec58
User13 <user13@umails.ru>
2018-07-19 00:01:57 +0300

    add some text

--- projectZero/Controllers/HomeController.cs
+++ projectZero/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace projectZero.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application new description page.";
 
             return View();
         }
